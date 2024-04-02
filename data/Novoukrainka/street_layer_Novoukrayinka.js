4ba689dcd0024c3b687d58b49c93b799bb116b1e
arhitektura2015 <165663281+arhitektura2015@users.noreply.github.com>
2024-04-02 16:08:07 +0300

    Add files via upload

---
+++ data/Novoukrainka/street_layer_Novoukrayinka.js
@@ -0,0 +1,8 @@
+var street_layer_Novoukrayinka ={
+"type": "FeatureCollection",
+"name": "streets_layer_Novoukrainka_pereimen",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "str_id": "899801", "str_type": 1, "codifier": "UA14160210230093332", "name_old": null, "str_name": "Вишнева", "note": null, "additional": null, "name_city": "Новоукраїнка" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 37.192449844, 48.227039856 ], [ 37.193193937, 48.227089758 ], [ 37.19361842, 48.227152968 ], [ 37.194951008, 48.227271389 ], [ 37.196086167, 48.227387999 ], [ 37.197131002, 48.227490521 ], [ 37.197699298, 48.227549031 ], [ 37.198208677, 48.227595607 ], [ 37.198508312, 48.227615568 ], [ 37.199117569, 48.227645509 ], [ 37.199991503, 48.227732006 ], [ 37.200883625, 48.227804931 ], [ 37.201808757, 48.2278881 ], [ 37.202748683, 48.227964617 ] ] ] } }
+]
+}
