74df1f68199a4753900cd884a61e96d2acf1efef
arhitektura2015 <165663281+arhitektura2015@users.noreply.github.com>
2024-04-02 08:51:23 +0300

    Add files via upload

---
+++ data/Pushkine/boundaries_layer_Pushkine.js
@@ -0,0 +1,8 @@
+var boundaries_layer_Pushkine ={
+"type": "FeatureCollection",
+"name": "межа населенного пункту с",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::5561" } },
+"features": [
+{ "type": "Feature", "properties": { "katottg": "UA14160210400096839", "settlement_name": "Пушкіне" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 37.138522475000059, 48.138479964000055 ], [ 37.144606326000087, 48.139064059000077 ], [ 37.146121791000041, 48.131930337000085 ], [ 37.139818070000047, 48.131137428000045 ], [ 37.138522475000059, 48.138479964000055 ] ] ] ] } }
+]
+}
