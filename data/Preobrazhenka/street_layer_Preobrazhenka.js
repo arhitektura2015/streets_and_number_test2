b07d78e712972ff59d7d8f9b2cffcb51c579836f
arhitektura2015 <165663281+arhitektura2015@users.noreply.github.com>
2024-04-02 16:09:51 +0300

    Add files via upload

---
+++ data/Preobrazhenka/street_layer_Preobrazhenka.js
@@ -0,0 +1,8 @@
+var street_layer_Preobrazhenka = {
+"type": "FeatureCollection",
+"name": "streets_layer_Preobrazhenka_pereimen",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "str_id": "{83221CC1-A3D1-484C-97F3-7187F97B5CAA}", "str_type": 1, "codifier": "UA14160210250050720", "name_old": null, "str_name": "Затишна", "note": null, "additional": null, "name_city": "Преображенка" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 36.97189, 48.11662 ], [ 36.97344, 48.11644 ], [ 36.97567, 48.11616 ], [ 36.97856, 48.11578 ], [ 36.98194, 48.11535 ], [ 36.98304, 48.11519 ], [ 36.98327, 48.11516 ], [ 36.98584, 48.11543 ], [ 36.98821, 48.11568 ], [ 36.9886, 48.11572 ], [ 36.9887, 48.11573 ], [ 36.98881, 48.11569 ], [ 36.98886, 48.11558 ] ] ] } }
+]
+}
