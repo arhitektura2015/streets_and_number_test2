7fed81cfcf2822fa55c7b1253fa3b09c65b49cb5
arhitektura2015 <165663281+arhitektura2015@users.noreply.github.com>
2024-05-31 10:10:37 +0300

    Add files via upload

---
+++ data/Vovkove/street_layer_Vovkove.js
@@ -0,0 +1,9 @@
+var street_layer_Vovkove
+ = {
+"type": "FeatureCollection",
+"name": "street_f434490d-5407-4c4d-a26b-2fa2cbbbb031",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "str_id": "{c70d60e7-b1b8-49f1-8c50-a6b21a8a5632}", "str_type": 1, "codifier": "UA14160210050078942", "name_old": null, "str_name": "Козацька", "note": null, "additional": null, "cvk": null, "dms": null, "dpa": null, "ds": null, "edesb": null, "mvs": null, "nais": null, "oe": null, "ozpa": null, "pd": null, "uoma": "{c70d60e7-b1b8-49f1-8c50-a6b21a8a5632}" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 37.081011434, 48.226799409 ], [ 37.080959537, 48.227152418 ], [ 37.080921155, 48.227431365 ], [ 37.08088926, 48.227655171 ], [ 37.080847735, 48.227956493 ], [ 37.080787087, 48.228200632 ], [ 37.080724919, 48.228546613 ], [ 37.080609381, 48.229369939 ], [ 37.08021475, 48.231654764 ], [ 37.080122358, 48.232103593 ], [ 37.080008022, 48.232731224 ], [ 37.079971532, 48.232955031 ], [ 37.079918013, 48.233497518 ] ] ] } }
+]
+}
